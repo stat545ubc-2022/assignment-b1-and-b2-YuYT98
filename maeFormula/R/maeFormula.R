d0dba8c1928e129ac2463a0153ff64dcda8df636
YuYT98 <yuyt1998@gmail.com>
2022-11-18 16:09:07 -0800

    update package

--- maeFormula/R/maeFormula.R
+++ maeFormula/R/maeFormula.R
@@ -1,4 +1,21 @@
-mae_formula = function(predicted_list, actual_list){
+#' Get the mean absolute error
+#'
+#' The function maeFormula is to calculate the mean absolute error or MAE based on the predicted results and their actual values you give. It returns MAE if all inputs have the same length, no NA/NULL values and the type of both lists is numeric, otherwise the function will not work and return an error message.
+#'
+#'
+#' @param predicted_list the predicted results stored in a list without NA/NULL values. The reason why this parameter called predicted_list is that this is the list for predicted results.
+#' @param actual_list the actual values stored in a list without NA/NULL values. The reason why this parameter called actual_list is that this is the list for actual values.
+#'
+#' @return mae  the computed mean absolute error result if all inputs are correct.
+#'
+#' @export
+#'
+#' @examples
+#' predicted_list_1 = list(1,2,3)
+#' actual_list_1 = list(4,5,6)
+#' maeFormula(predicted_list_1, actual_list_1)
+
+maeFormula = function(predicted_list, actual_list){
 
   if(length(predicted_list) == length(actual_list) &&
      !anyNA(predicted_list, actual_list) &&
